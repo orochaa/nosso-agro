b0f7004412ec34c47b9fd481e138874b06eb5853
Bruno Rocha <brunorocha2674@gmail.com>
2025-06-13 19:03:43 -0300

    chore: add openapi spec file

--- src/main/server.ts
+++ src/main/server.ts
@@ -3,6 +3,8 @@ import { AppModule } from '#main/modules/app.module.js'
 import { ValidationPipe } from '#main/validation.pipe.js'
 import { HttpAdapterHost, NestFactory } from '@nestjs/core'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { existsSync, mkdirSync, writeFileSync } from 'node:fs'
+import path from 'node:path'
 
 // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing, @typescript-eslint/no-magic-numbers
 const port = process.env.PORT || 3000
@@ -37,6 +39,16 @@ async function bootstrap(): Promise<void> {
         .build()
     )
     SwaggerModule.setup('docs', app, document)
+
+    const docsFolder = path.resolve('docs')
+
+    if (!existsSync(docsFolder)) {
+      mkdirSync(docsFolder)
+    }
+    writeFileSync(
+      path.resolve(docsFolder, 'openapi-spec.json'),
+      JSON.stringify(document)
+    )
   }
 
   await app.listen(port)
